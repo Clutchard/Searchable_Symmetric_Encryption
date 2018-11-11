8f46440e68d7a8e969e2db78ec13a15b80c562e9
clutchard <rrn14@my.fsu.edu>
2018-11-10 19:20:01 -0500

    added comments to code

--- SSE.py
+++ SSE.py
@@ -1,7 +1,7 @@
 import itertools #used for permutations
 from cryptography.fernet import Fernet #used for the symmetric key generation
-from collections import Counter
-from collections import defaultdict
+from collections import Counter #used to count most common word
+from collections import defaultdict # used to make the the distinct word list
 
 
 
@@ -12,11 +12,13 @@ def main():
 	print("Welcome to Searchable Symmetric Encryption.\n\n")
 	reply = input("Do you already have an encrypted data set? (Y)es or (N)o: ")
 	while(True):
+		#if yes then you have already generated keys and just want to search
 		if(reply.lower() == 'y' or reply.lower() == "yes"):
 			s = input("Please enter your s key: ")
 			y = input("Please enter your y key: ")
 			z = input("Please enter your z key: ")
 			break
+		#if no then need to generate symmetric keys
 		elif (reply.lower() == 'n' or reply.lower() == "no"):
 			s = Fernet.generate_key()
 			y = Fernet.generate_key()
@@ -25,6 +27,10 @@ def main():
 			print("Here is your y key ", y.decode("ascii"))
 			print("Here is your z key ", z.decode("ascii"))
 			print("\n\nThese keys are very important must keep secret and must not lose!!!!\n\n")
+			#Generated keys and printed to screen
+
+
+			#Gets filenames from the user 
 			filenames = []
 			x = input("Please enter the name of a file you want to encrypt: ")
 			filenames.append(x)
@@ -33,6 +39,7 @@ def main():
 				if not x:
 					break
 				filenames.append(x)
+			# finds the occurence of each word in a flle
 			filedata = []
 			for idx, val in enumerate(filenames):
 				cnt = Counter()
@@ -42,25 +49,27 @@ def main():
 						cnt[word]+=1
 				filedata.append((val,cnt))
 			
+			#takes the 5 most common from each document as the distinct words
 			allwords = []
 			for idx, val in enumerate(filedata):
 				for value, count in val[1].most_common(5):
 					if  value not in allwords:
 						allwords.append(value)
 
+			#makes a dictory with the distinct word as index and a value of a list of filenames
 			word_dict = defaultdict(list)
 			for i in allwords:
 				word_dict[i]
 				for idx, val in enumerate(filedata):
 					if i in val[1]:
 						word_dict[i].append(val[0])
-
-			for i in word_dict.items():
-				print(i)
-			answer = input("What word you want to see")
-			print(word_dict[answer])
+			#uncomment out to see the word_dict list
+			#for i in word_dict.items():
+				#print(i)
 
 			break
+
+		#this just makes sure user enters yes or no or y or n
 		else:
 			reply = input("\nInput Y for yes or N for no: ")
 
