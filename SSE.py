284c8b05a7bebc8095f7fd26daa88b7ef64af9b3
Daniel Kane <daniel-kane@github.com>
2018-12-02 13:53:55 -0500

    Made it more user friendly, added comments, removed garbage
    Edited readme

--- SSE.py
+++ SSE.py
@@ -1,3 +1,18 @@
+'''
+Searchable Symmetric Encryption Scheme
+
+Python Version: 3.6
+Dependency: cryptography module (see README)
+
+Implemented by Kobra Amirizirtol, Daniel Kane, Richard Nelson
+
+Based on the work by Reza Curtmola, Seny Kamara, Juan Garay
+and Rafail Ostrovsky in 'Searchable Symmetric Encryption: Improved
+Definitions and Efficient Constructions'
+
+'''
+
+
 import itertools 
 from itertools import permutations, combinations #used for permutations
 from cryptography.fernet import Fernet #used for the symmetric key generation
@@ -15,9 +30,6 @@ import re
 import bitarray #for lookup table
 
 
-
-
-
 def main():
 	print("Welcome to Searchable Symmetric Encryption.\n\n")
 	reply = input("Do you already have an encrypted data set? (Y)es or (N)o: ")
@@ -26,7 +38,6 @@ def main():
 		if(reply.lower() == 'y' or reply.lower() == "yes"):
 			password = input("Please enter the password: ")
 			break
-
 
 		#if no then need to generate symmetric keys
 		elif (reply.lower() == 'n' or reply.lower() == "no"):
@@ -41,36 +52,31 @@ def main():
 				print("Passwords not the same try again\n")
 
 			key_s, key_y, key_z = keygen(password)
-			#uncomment out to see the three keys
-			#print(key_s)
-			#print(key_y)
-			#print(key_z)
 
 			word_dict = intialization()
-			#uncomment out to see the word_dict list
-			#for i in word_dict.items():
-			#	print(i)
 
-			arr_of_linked_lists, keyword_key_pair = build_array(word_dict, key_s, key_y, key_z)
-
-			#for i in arr_of_linked_lists:
-				#print(i)
-			#You can tell the difference between the indexes by length
+			A, keyword_key_pair = build_array(word_dict, key_s, key_y, key_z)
 
 			T = look_up_table(keyword_key_pair, key_s, key_y, key_z)
-			#for i in T:
-			#	print(T)
 
-			keyword = input("Please enter the keyword to search: ")
-			
-			trapdoor = Trapdoor(keyword, key_z, key_y)		
+			print("\n\nWelcome!")
+			keyword = input("\nPlease enter the keyword to search, or 'exit' to exit: ")
 
-			list_of_docs = Search(T, arr_of_linked_lists, trapdoor)
+			while keyword != 'exit':
+				
+				trapdoor = Trapdoor(keyword, key_z, key_y)		
 
-			#print(list_of_docs)
+				list_of_docs = Search(T, A, trapdoor)
+
+				print(f"\nSearch Results for \"{keyword}\":\n")
+				for i in list_of_docs:
+					print(i)
+
+				keyword = input("\nPlease enter the keyword to search, or 'exit' to exit: ")
+
+			print("\n\nGoodbye!\n")
 
 			break
-
 
 		#this just makes sure user enters yes or no or y or n
 		else:
@@ -81,6 +87,12 @@ def main():
 ############################################################################################
 
 def intialization():
+	''' 
+	Prompts user for documents to be encrypted and generates the distinct 
+	words in each. Returns the distinct words and the documents that contained them
+	in a dictionary 'word_dict'
+	'''
+
 	filenames = []
 	x = input("Please enter the name of a file you want to encrypt: ")
 	filenames.append(x)
@@ -105,7 +117,8 @@ def intialization():
 		for value, count in val[1].most_common(5):
 			if  value not in allwords:
 				allwords.append(value)
-		#makes a dictory with the distinct word as index and a value of a list of filenames
+
+	#makes a dictory with the distinct word as index and a value of a list of filenames
 	word_dict = defaultdict(list)
 	for i in allwords:
 		word_dict[i]
@@ -113,18 +126,24 @@ def intialization():
 			if i in val[1]:
 				word_dict[i].append(val[0])
 		word_dict[i].sort()
+
 	return word_dict
-
-
 
 ############################################################################################
 
 def keygen(u_password):
-	password_provided = u_password # This is input in the form of a string
-	password = password_provided.encode() # Convert to type bytes
-	salt_s = b'\x91\xabr\xebx\xc5\x9dx^b_7\xb6\x8a\xbb5' # CHANGE THIS - recommend using a key from os.urandom(16), must be of type bytes
+	''' Generates 3 keys, key s,y,z, based on the given password from the user. '''
+
+	# This is input in the form of a string
+	password_provided = u_password 
+
+	# Convert to type bytes
+	password = password_provided.encode() 
+
+	salt_s = b'\x91\xabr\xebx\xc5\x9dx^b_7\xb6\x8a\xbb5'
 	salt_y = b'\x1cy8\r\x7f\xf8,\xe2Pu!/\x043\xdc\x0e'
 	salt_z = b'\x9b\xd0\xb6\x85!J\xde\xe5\xc8\xb3\xc9\xa2\tqPy'
+
 	kdf_s = PBKDF2HMAC(
 	    algorithm=hashes.SHA256(),
 	    length=32,
@@ -158,49 +177,47 @@ def keygen(u_password):
 
 ############################################################################################
 def psuedo_random(key_s, ctr):
+	''' A pseudorandom function based on key s, to return a value to index array A '''
+
+	#Convert key s to decimal value
 	decimal_key = int.from_bytes(key_s, byteorder=sys.byteorder)
 	combined = decimal_key + ctr
+
+	#Find a random value based on key s and counter
 	random.seed(combined)
 	index = random.randrange(0, 10000)
 	return index
 
-
 ############################################################################################
 def build_array(word_dict, key_s, key_y, key_z):
-	#print("Build array Part")
-	#initialize empty list A. A will be the array of linked lists, or in this case list of linked lists
-	#can append each linked list after it is created below
+	'''
+	Creates an array of nodes, each containing the document id, key to encrypt the
+	next node, and the address of the next node
+	'''
+
 	A = [0] * 10000
 	ctr = 1
 	keyword_key_pair = []
 
-
 	#for each word in set of distinct words, word_dict in this case	
 	for i, doc_list in word_dict.items():
 
+		#Generate a key for the first node
 		K_i_0 = Fernet.generate_key()
 		keyword_key_pair.append([i, K_i_0, ctr])
+
+		#initialize the previous key to the first one created
+		K_i_jminus1 = K_i_0 
 		
 		# for 1 <= j <= |D(wi)|:
 		# for each document which has distinct word wi....iterate through doc_list
-		K_i_jminus1 = K_i_0 #initialize the previous key to the first one created
-		
 		for j, doc in enumerate(doc_list):
 			
-			#again generate key K(i,j) lenght l?
+			#again generate key K(i,j)
 			K_i_j = Fernet.generate_key()
 
-		
-
-			#print("current key: " + str(K_i_j))
-			#print("previous key: " + str(K_i_jminus1))
-			
-
 			#N(i,j) = (id(D(i,j) || K(i,j) || v(s)(ctr+1)), where id(D(i,j) is the jth identifier in D(wi)
-			# first part is document id, second is the key used to encrypt the next node, 
-				#and v(s) is the address of the next node...
-			#are te document identifiers just their names?
-			#N = K_i_j
+	
 			curr_addr = psuedo_random(key_s, ctr)
 			if j == len(doc_list) - 1:
 				next_addr = None
@@ -208,34 +225,18 @@ def build_array(word_dict, key_s, key_y, key_z):
 				next_addr = psuedo_random(key_s, ctr+1)
 			
 			N = doc + "\n" + str(K_i_j) + "\n" + str(next_addr)
-			#newline is a delimeter to seperate three components of the encrypted string
+			#newline is a delimeter to separate three components of the encrypted string
 			#N = doc + K_i_j + address of next node. 
 
 			#encrypt N with Ki,j-1, ie the previous key
-			#something like the folliwng maybe?
-
 			N = Fernet(K_i_jminus1).encrypt(str.encode(N))
 
-			#print("Encrypted n: " + str(N))
-			#ptext = Fernet(K_i_jminus1).decrypt(N)
-			
-			#print("Decrypted n: " + str(ptext))
+			#update and save K at i,j-1
+			K_i_jminus1 = K_i_j 
 
-			#print("K i,j-1" + str(K_i_jminus1))
-			#print("K_i_j" + str(K_i_j))
-			K_i_jminus1 = K_i_j #update and save K at i,j-1
-
-			#store the encrypted N in the array here?
-			#A[v(ctr)] = result
+			#store the encrypted N in the array
 			A[curr_addr] = N
-			#print(A[curr_addr])
-
-			#How to decrypt the node into the three componets 
-			#y = A[curr_addr]
-			#new_n = Fernet(K_i_jminus1).decrypt(y)
-			#z = new_n.decode()
-			#print(z)
-
+	
 			#update counter
 			ctr = ctr + 1
 
@@ -252,6 +253,10 @@ def build_array(word_dict, key_s, key_y, key_z):
 
 ############################################################################################
 def look_up_table(keyword_key_pair,key_s, key_y, key_z):
+	'''
+	Generates a table which stores the XORed result of permutation
+	function f_y and the address of a node concatenated with the key
+	'''
 	T = [0] * 1000
 	for i in keyword_key_pair:
 		keyword = i[0]
@@ -278,8 +283,6 @@ def look_up_table(keyword_key_pair,key_s, key_y, key_z):
 
 		value = [f_y ^ x for x in cat_string]
 
-		
-	
 		T[index] = value
 	
 
@@ -293,6 +296,10 @@ def look_up_table(keyword_key_pair,key_s, key_y, key_z):
 	return T
 
 def Trapdoor(keyword, key_z, key_y):
+	'''
+	returns the permutation function
+	and pseudorandom permutation function on keyword
+	'''
 	random.seed(keyword + str(key_z))
 	index = random.randrange(0, 1000)
 	
@@ -304,115 +311,71 @@ def Trapdoor(keyword, key_z, key_y):
 
 
 def Search(T, A, trapdoor):
+	'''
+	Indexes both T and A with trapdoor values generated by keyword 
+	in main to find and decrypt the document ids
+	'''
+
+	list_of_docs = []
 
 	value = T[trapdoor[0]]
-	#print(value)
-	#print(trapdoor[1])
-
-	#f_y = bitarray.bitarray(1000)
-	#f_y.setall(False)
-	#f_y.frombytes(str(trapdoor[1]).encode('utf-8'))
 
 	f_y = trapdoor[1]
 
+	#XORs the ascii value with f_y to obtain list version of string 
+	#	containing the address and the key for the node
 	addr_and_key = [chr(f_y ^ x) for x in value]
-	#print(node)
-
 	
+	#converts the list into one string
 	mystring = ''
 	for x in addr_and_key:
 		mystring = mystring + x
 
+	#addr_node is a list.
 	addr_node = re.split(r"\n", str(mystring))
 
+	#if addr_node isn't two separate items, we didn't find a document
 	if len(addr_node) == 1:
-		print("did not find anything")
+		print("\n")
 	else:
 
 		addr = addr_node[0]
 		key = addr_node[1]
+
+		#remove b' at the beginning and ' at the end
 		key = key[2:-1]
 
+		#get the node based on the address from array A
 		node = A[int(addr)]
 
-
+		#turn key back into bytes and use Fernet function to 
+		#	decrypt back to plaintext
 		decrypted_node = Fernet(str.encode(key)).decrypt(node)
 		
+		#remove b' at the beginning and ' at the end
 		d_n = str(decrypted_node)[2:-1]
 		split_node = re.split(r"\\n", d_n)
 		doc_id = split_node[0]
 		key = split_node[1]
 		addr = split_node[2]
 
-		print("Search Results: \n")
-		print(doc_id)
+		list_of_docs.append(doc_id)
 
+		#Repeat iterating while the address is not null, meaning
+		#	 there are still documents with the keyword
 		while addr != 'None':
-			
 			key = key[2:-1]
 			key = str.encode(key)
-
 			node = A[int(addr)]
 			decrypted_node = Fernet(key).decrypt(node)
-	
-	
 			d_n = str(decrypted_node)[2:-1]
-
-
 			split_node = re.split(r"\\n", d_n)
-
-	
 			doc_id = split_node[0]
 			key = split_node[1]
 			addr = split_node[2]
-			
-			print(doc_id)
+			list_of_docs.append(doc_id)
 
-		
-			
-
-
-	
-
-	
-
-	#node = bitarray.bitarray(1000)
-	#node.setall(False)
-	#node.frombytes(bytes(x ^ y for x, y in zip(f_y, value)))
-	
-	#print(type(bitarray.bitarray(node).tobytes().decode('utf-8')))
-
-	#print(bitarray.bitarray(node).tobytes())
-	#print(bitarray.bitarray(node).tobytes().decode('utf-8'))
-	#print(bitarray.bitarray(node).tobytes().decode('ascii'))
-
-	#string = str(bitarray.bitarray(node).tobytes().decode('utf-8'))
-
-	#string = string.decode('utf-8')
-	#print(string)
-
-	#node = [x ^ y for x,y in zip(f_y,value)]
-
-	#print(node)
-	#print(frombits(node))
-	#print(type(bitarray.bitarray(node).tobytes().decode('utf-8')))
-
-	#print(str(bitarray.bitarray(node).tobytes().decode('utf-8')))
-	
-	#bitarray.bitarray(node).tobytes().decode('utf-8')
-
-	#for i in node:
-	#	if i 
-	
-	#addr_node = re.split(r"\n", str(node))
-
-	#print(addr_node)
-
-
-
-	
-
-
+	return list_of_docs
 
 if __name__ == '__main__':
 	main()
